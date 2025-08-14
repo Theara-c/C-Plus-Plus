d86c501968b2296df55f2ff109cbff70c4b26bd1
Theara-c <longsotheara008@gmail.com>
2025-08-14 12:57:42 +0700

    w2

---
+++ ex3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std; 
+
+int factorialIter ( int n){
+    int result = 1;
+    if( n == 0){
+    return result;
+    }
+ for( int i = n; i> 0; i--){
+    result *= i;
+ }
+ return result;
+}
+int factorialRec ( int n){
+
+    if( n <=1 ){
+        return 1;
+    }
+    if (n > 1){
+        return n * factorialRec(n-1);
+    }
+
+    return 1;
+}
+
+int main (){
+    int n = 5; 
+    cout << n << "! iterative = " << factorialIter( n) << "\n";
+    cout << n << "! recursive = " << factorialRec (n) << "\n";
+    return 0;
+}
---
+++ ex4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int sumRow(const int m[row][3], int row){
+
+}
+int sumCol (const int m[][], int col){
+
+}
+
+int main (){
+    int mat[3][3] = { 
+{ 1, 2, 3 }, 
+{ 4, 5, 6 }, 
+{ 7, 8, 9 } 
+}; 
+    return 0;
+
+}
